f10ac251916867e6b302440e4d8226ac58404ea8
Luke Schoen <ltfschoen@gmail.com>
2016-07-08 01:08:28 +1000

    Fixed tslint error

--- process/typescript/book-special.interface.ts
+++ process/typescript/book-special.interface.ts
@@ -1,6 +1,6 @@
 // Interface (Class without any methods) without data details
 export interface BookSpecialInterface {
-	name: string;
+  name: string;
   number: string;
   shortname: string;
   addressLine1: string;
